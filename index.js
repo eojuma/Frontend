6ee4c41f578b3fb3535b54fbce259b9880a8ec9a
eojuma <evansjuma1e@gmail.com>
2025-08-29 11:21:08 +0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Hello world!")
+
+//Javascript statements
+
+let a = 5;
+let b = 10;
+let sum = a + b;
+console.log(sum)
+
+x=5;y=7;z=x+y;
+console.log(z)
+
+//I will keep building on this language until i master it
